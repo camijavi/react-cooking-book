7a9bb369d34951733de63ff31289a5e29e91fbbc
camijavi <camijavinavarro@gmail.com>
2026-03-04 17:23:59 -0600

    Add new image files for the project

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -10,11 +10,13 @@ import {
   Vector3,
 } from "three";
 import { pages } from "./UI";
+import { useHelper, useTexture } from "@react-three/drei";
+import { useFrame } from "@react-three/fiber";
 
 const PAGE_WIDTH = 1.28;
 const PAGE_HEIGHT = 1.71;
 const PAGE_DEPTH = 0.003;
-const PAGE_SEGMENTS = 32;
+const PAGE_SEGMENTS = 30;
 const SEGMENT_WIDTH = PAGE_WIDTH / PAGE_SEGMENTS;
 
 const pageGeometry = new BoxGeometry(
@@ -66,15 +68,16 @@ const pageMaterials = [
   new MeshStandardMaterial({
     color: "white",
   }),
-  new MeshStandardMaterial({
-    color: "pink",
-  }),
-  new MeshStandardMaterial({
-    color: "blue",
-  }),
 ];
 
 const Page = ({ number, front, back, ...props }) => {
+  const [picture, pictureRoughness, picture2] = useTexture([
+    `/textures/${front}.jpg`,
+    `/textures/${back}.jpg`,
+    ...(number === 0 || number === pages.length
+      ? [`/textures/book-cover-roughness.jpg`]
+      : []),
+  ]);
   const group = useRef();
 
   const skinnedMeshRef = useRef();
@@ -92,14 +95,48 @@ const Page = ({ number, front, back, ...props }) => {
       }
     }
     const skeleton = new Skeleton(bones);
-    const mesh = new SkinnedMesh(pageGeometry, pageMaterials);
+    const materials = [
+      ...pageMaterials,
+      new MeshStandardMaterial({
+        color: "white",
+        map: picture,
+        ...(number === 0
+          ? {
+              roughnessMap: pictureRoughness,
+            }
+          : {
+              roughness: 0.1,
+            }),
+      }),
+      new MeshStandardMaterial({
+        color: "white",
+        map: picture2,
+        ...(number === pages.length - 1
+          ? {
+              roughnessMap: pictureRoughness,
+            }
+          : {
+              roughness: 0.1,
+            }),
+      }),
+    ];
+    const mesh = new SkinnedMesh(pageGeometry, materials);
     mesh.castShadow = true;
     mesh.receiveShadow = true;
     mesh.frustumCulled = false;
     mesh.add(skeleton.bones[0]);
     mesh.bind(skeleton);
     return mesh;
-  }, []);
+  }, [number, picture, picture2, pictureRoughness]);
+
+  //   useHelper(skinnedMeshRef, SkeletonHelper, "cyan");
+
+  useFrame(() => {
+    if (!skinnedMeshRef.current) {
+      return;
+    }
+    const bones = skinnedMeshRef.current.skeleton.bones;
+  });
 
   return (
     <group {...props}>
