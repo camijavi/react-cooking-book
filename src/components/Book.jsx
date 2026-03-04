c1ecde821c3e9c1a2c53c36268fd77400b618424
camijavi <camijavinavarro@gmail.com>
2026-03-04 14:52:38 -0600

    Add Book component and integrate it into Experience

---
+++ src/components/Book.jsx
@@ -0,0 +1,126 @@
+import { useMemo, useRef } from "react";
+import {
+  Bone,
+  BoxGeometry,
+  Float32BufferAttribute,
+  MeshStandardMaterial,
+  Skeleton,
+  SkinnedMesh,
+  Uint16BufferAttribute,
+  Vector3,
+} from "three";
+import { pages } from "./UI";
+
+const PAGE_WIDTH = 1.28;
+const PAGE_HEIGHT = 1.71;
+const PAGE_DEPTH = 0.003;
+const PAGE_SEGMENTS = 32;
+const SEGMENT_WIDTH = PAGE_WIDTH / PAGE_SEGMENTS;
+
+const pageGeometry = new BoxGeometry(
+  PAGE_WIDTH,
+  PAGE_HEIGHT,
+  PAGE_DEPTH,
+  PAGE_SEGMENTS,
+  2
+);
+
+pageGeometry.translate(PAGE_WIDTH / 2, 0, 0);
+
+const position = pageGeometry.attributes.position;
+const vertex = new Vector3();
+const skinIndexes = [];
+const skinWeights = [];
+
+for (let i = 0; i < position.count; i++) {
+  vertex.fromBufferAttribute(position, i);
+  const x = vertex.x;
+
+  const skinIndex = Math.max(0, Math.floor(x / SEGMENT_WIDTH));
+  const skinWeight = (x % SEGMENT_WIDTH) / SEGMENT_WIDTH;
+
+  skinIndexes.push(skinIndex, skinIndex + 1, 0, 0);
+  skinWeights.push(1 - skinWeight, skinWeight, 0, 0);
+}
+
+pageGeometry.setAttribute(
+  "skinIndex",
+  new Uint16BufferAttribute(skinIndexes, 4)
+);
+
+pageGeometry.setAttribute(
+  "skinWeight",
+  new Float32BufferAttribute(skinWeights, 4)
+);
+
+const pageMaterials = [
+  new MeshStandardMaterial({
+    color: "white",
+  }),
+  new MeshStandardMaterial({
+    color: "#111",
+  }),
+  new MeshStandardMaterial({
+    color: "white",
+  }),
+  new MeshStandardMaterial({
+    color: "white",
+  }),
+  new MeshStandardMaterial({
+    color: "pink",
+  }),
+  new MeshStandardMaterial({
+    color: "blue",
+  }),
+];
+
+const Page = ({ number, front, back, ...props }) => {
+  const group = useRef();
+
+  const skinnedMeshRef = useRef();
+
+  const manualSkinnedMesh = useMemo(() => {
+    const bones = [];
+    for (let i = 0; i <= PAGE_SEGMENTS; i++) {
+      const bone = new Bone();
+      bones.push(bone);
+      if (i === 0) {
+        bone.position.x = 0;
+      } else {
+        bone.position.x = SEGMENT_WIDTH;
+        bones[i - 1].add(bone);
+      }
+    }
+    const skeleton = new Skeleton(bones);
+    const mesh = new SkinnedMesh(pageGeometry, pageMaterials);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.frustumCulled = false;
+    mesh.add(skeleton.bones[0]);
+    mesh.bind(skeleton);
+    return mesh;
+  }, []);
+
+  return (
+    <group {...props}>
+      <primitive object={manualSkinnedMesh} ref={skinnedMeshRef} />
+    </group>
+  );
+};
+
+export const Book = ({ ...props }) => {
+  return (
+    <group {...props}>
+      {[...pages].map((pageData, index) =>
+        index === 0 ? (
+          <Page
+            position={[0, 0, index * 0.42]}
+            key={index}
+            number={index}
+            {...pageData}
+          />
+        ) : null
+      )}
+    </group>
+  );
+};
